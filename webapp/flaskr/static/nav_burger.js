f9daf78f3910aca44ace200fc4faba7fde7105fe
shademel <shdem0192@gmail.com>
2023-04-09 22:41:11 +1000

    Create nav_burger.js

---
+++ webapp/flaskr/static/nav_burger.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    if(navbarBurgers.length > 0) {
+        navbarBurgers.array.forEach(function (item) {
+            item.addEventListener('click', function() {
+                var target = item.dataset.target;
+                var target = document.getElementById(target);
+
+                item.classList.toggle('is-active');
+                target.classList.toggle('is-active');
+            });
+        });
+    }
+});
