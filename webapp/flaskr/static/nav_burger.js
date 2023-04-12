a0fd61fc72131981870352e737461c846e51084e
shademel <shdem0192@gmail.com>
2023-04-12 19:50:28 +1000

    Update nav_burger.js

--- webapp/flaskr/static/nav_burger.js
+++ webapp/flaskr/static/nav_burger.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    
     if(navbarBurgers.length > 0) {
         navbarBurgers.array.forEach(function (item) {
             item.addEventListener('click', function() {
